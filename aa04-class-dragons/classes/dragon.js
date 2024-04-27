08ed72a0b67d9402f5047c0ff369f74babc08b29
Ruben Cuello <kuelmusic@gmail.com>
2024-04-27 12:49:32 -0400

    01

--- aa04-class-dragons/classes/dragon.js
+++ aa04-class-dragons/classes/dragon.js
@@ -1,4 +1,20 @@
-// Your code here 
+class Dragon{
+  constructor(name, color){
+    this.name = name
+    this.color = color
+  }
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragons){
+    // let dragonArr = []
+    // for(let dragon of dragons){
+    //   dragonArr.push(dragon.name)
+    // }
+    // return dragonArr
+    return dragons.map((dragon) => dragon.name)
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
